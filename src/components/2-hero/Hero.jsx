02cd6ea24157f8338d9443866a79882c1986272b
Simohjc <mohamed.elkhair87@gmail.com>
2025-05-27 17:41:46 -0700

    update

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -3,9 +3,26 @@ import './Hero.css'; // Assuming you have a CSS file for styling
 
 const Hero = () => {
   return (
-    <div>
-      hero
-    </div>
+    <section className='hero flex'>
+       <div className='left-side'>
+        <div className='parent-simo'>
+          <img src="./simo.png" className='heropic' alt="mohamed-elkhair"/>
+          <span className='icon-verified'></span>
+        </div>
+        <h1 className='title'>Web developer, ReactJs</h1>
+        <p className='sub-title'>I’m a versatile professional with a background as a Computer and Data Management Specialist, 
+              IT Operations Specialist, Technical Support Specialist, and Business Development Executive. Over the years, 
+                I’ve developed a strong foundation in technology, problem-solving, and client relations.
+        </p>
+        <div className='icons'>
+          <span className='icon-instagram icon-instagram'></span>
+          <span className='icon-twitter icon-twitter'></span>
+          <span className='icon-github icon-github'></span>
+          <span className='icon-linkedin icon-linkedin'></span>
+        </div>
+       </div>
+       <div className='right-side animation border'>animation</div>
+    </section>
   )
 }
 
--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
 import './Main.css'; // Assuming you have a CSS file for styling
 
 const Main = () => {
   return (
-    <div>
-      header
-    </div>
+    <main className='flex'>
+      <section className='left-section flex'>
+             <button className='active'>All Projects</button>
+             <button>HTML & CSS</button>
+             <button>JavaScript</button>
+             <button>React & MUI</button>
+             <button>Node & Express</button>
+      </section>
+      <section className='right-section flex'>
+
+              {["aa", "bb", "cc", "dd", "ff", "gg", "ss","ll"].map((item) => (
+                  <article  key={item} className='card'>
+                   <img src='./ecommerce.png' alt="ecommerce" width={266} className='ecommerce-img' />
+                   <div className='box' style={{width:"266px"}}>
+                      <h1 className='title'>E-Commerce Website</h1>
+                      <p className='subtitle'>A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.</p>
+                      <div className='flex icons'>
+                       <div className='flex' style={{gap: "11px"}}>
+                         <div className='icon-link'></div>
+                         <div className='icon-github'></div>
+                       </div>
+                       <a href='hhhh' className='link flex'>
+                         more 
+                         <span style={{alignSelf:"end"}} className='icon-arrow-right'></span>
+                       </a>
+                      </div>
+                   </div>
+                 </article>
+              ))}
+      </section>
+    </main>
+
   )
 }
 
